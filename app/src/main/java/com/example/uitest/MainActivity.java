4959919d560ef8c6d120eb51ce5941ef631b0bb3
HC <329806042@qq.com>
2019-06-09 10:51:28 +0800

    3.4.1

--- app/src/main/java/com/example/uitest/MainActivity.java
+++ app/src/main/java/com/example/uitest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.uitest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,48 +12,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private EditText editText;
-    private ImageView imageView;
-    private ProgressBar progressBar;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.button_1);
-        editText = (EditText)findViewById(R.id.edit_text);
-        imageView = (ImageView)findViewById(R.id.image_view);
-        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
-        button.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.button_1:
-                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("this is a dialod");
-                dialog.setMessage("it is mot important");
-                dialog.setCancelable(false);
-                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-                dialog.show();
-                break;
-                default:
-                    break;
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
         }
     }
+
 }
